f1286aa3f46751d5dd0625750841bf02df6e81ac
absenth <wintermute223@gmail.com>
2021-05-24 00:35:01 -0500

    Database setup

--- db.py
+++ db.py
@@ -2,3 +2,20 @@
 When setup.py calls us, we should set up the database for this year's
 ARRL Field Day contest.
 """
+
+import sqlite3
+import datetime
+
+# Set the database name based on this year.
+dbname=("agridieslog-"+str(datetime.date.today().year)+".db")
+
+# set up the database for this year's field day
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
+
+c.execute('''CREATE TABLE LOGS
+         ([generate_id] INTEGER PRIMARY KEY,[band] text, [mode] text,
+         [ocall] text, [ocat] text, [osec] text, [tcall] text, [tcat] text,
+         [tsec] text, [datetime] text)''')
+
+
