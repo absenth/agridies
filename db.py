b174617406db7db71c4bb661a41eed3265b3122c
absenth <wintermute223@gmail.com>
2021-05-24 08:49:39 -0500

    adding logshow and starting logexport

--- db.py
+++ db.py
@@ -8,16 +8,17 @@ from datetime import datetime, tzinfo
 
 # Set the database name based on this year.
 dbname=("agridieslog-"+str(datetime.date.today().year)+".db")
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
 
 
 def dbsetup():
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-
     c.execute('''CREATE TABLE LOGS
              ([id] INTEGER PRIMARY KEY,[band] text, [mode] text,
              [ocall] text, [ocat] text, [osec] text, [tcall] text, [tcat] text,
              [tsec] text, [dt] timestamp)''')
+
+    c.commit()
 
     '''
     LOGS - table rows:
@@ -25,8 +26,17 @@ def dbsetup():
     '''
 
 def logwrite():
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-
     dt = datetime.utcnow()
     c.execute("INSERT INTO LOGS VALUES (band, mode, ocall, ocat, osec, tcall, tcat, tsec, dt)")
+
+def logshow():
+    c.execute("SELECT * FROM LOGS")
+    print(cur.fetchall())
+
+def logexport():
+    '''
+    This should create the Cabrillo format export of the logs
+    I should figure out how to make DT look like "2021-06-26 1314"
+    Or perhaps create a date, and a time table, and only insert
+    the simplified values.
+    '''
