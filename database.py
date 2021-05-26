0a97720e85f804e4f43d424ce367d7551381fb13
absenth <wintermute223@gmail.com>
2021-05-26 09:02:35 -0500

    setting up the Database Class

--- database.py
+++ database.py
@@ -10,11 +10,13 @@ from datetime import datetime, tzinfo
 
 class DataBase:
 
-    def dbsetup():
+    def __init__(self):
         dbname=("fielddaylog-"+str(datetime.today().year)+".db")
-        conn = sqlite3.connect(dbname)
-        c = conn.cursor()
-        c.execute('''CREATE TABLE LOGS
+        self.conn = sqlite3.connect(dbname)
+
+    def dbsetup(self):
+        self.conn()
+        self.conn.cursor().execute('''CREATE TABLE LOGS
                  ([id] INTEGER PRIMARY KEY NOT NULL,
                  [band] text, [mode] text, [ocall] text, [ocat] text, [osec] text,
                  [tcall] text, [tcat] text, [tsec] text, [dt] timestamp)''')
@@ -24,29 +26,25 @@ class DataBase:
             band - mode - ocall - ocat - osec - tcall - tcat - tsec - dt
         '''
 
-    def logwrite():
-        dbname=("fielddaylog-"+str(datetime.today().year)+".db")
-        conn = sqlite3.connect(dbname)
-        c = conn.cursor()
+    def logwrite(self):
         DT = datetime.utcnow()
-        c.execute('''INSERT INTO LOGS (band, mode, ocall, ocat, osec, tcall, tcat,
+        self.conn.cursor().execute('''INSERT INTO LOGS (band, mode, ocall, ocat, osec, tcall, tcat,
                  tsec, dt) VALUES (Band, Mode, Ocall, Ocat, Osec, Tcall, Tcat,
                  Tsec, DT)''')
 
-    def logshow():
-        dbname=("fielddaylog-"+str(datetime.today().year)+".db")
-        conn = sqlite3.connect(dbname)
-        c = conn.cursor()
+    def logshow(self):
+        c = self.conn.cursor()
         c.execute("SELECT * FROM LOGS")
         print(c.fetchall())
 
-    def logexport():
-        dbname=("fielddaylog-"+str(datetime.today().year)+".db")
-        conn = sqlite3.connect(dbname)
-        c = conn.cursor()
+    def logexport(self):
+        c = self.conn.cursor()
+        c.execute(##FIXME##)
+
         '''
         This should create the Cabrillo format export of the logs
         I should figure out how to make DT look like "2021-06-26 1314"
         Or perhaps create a date, and a time table, and only insert
         the simplified values.
         '''
+
